92edafc0446c506b8d8a270480c3d33f4710c6e5
elena-andonova <ellie.andonova@gmail.com>
2016-02-16 22:12:34 +0200

    backend-services test successful

--- BirthdayReminder/app/main-view-model.js
+++ BirthdayReminder/app/main-view-model.js
@@ -5,16 +5,27 @@ var HelloWorldModel = (function (_super) {
         _super.call(this);
         this.counter = 42;
         this.set("message", this.counter + " taps left");
-
+        this.set("ms",'Data taken!');
         var places = global.everlive.data('Custom_Users');
 
-places.get(null, function(data) {
-        this.set("ms",JSON.stringify(data));
-}, function(err) {
-    alert(err.message);
-});
+        places.get(null, function(data) {
+            console.log(JSON.stringify(data));
+             this.set("ms",'In function!');
+        }, function(err) {
+            console.log(err.message);
+        });
     }
+
     HelloWorldModel.prototype.tapAction = function () {
+        var places = global.everlive.data('Custom_Users');
+
+        places.get(null, function(data) {
+            console.log(JSON.stringify(data));
+             this.set("ms",JSON.stringify(data));
+        }, function(err) {
+            console.log(err.message);
+        });
+
         this.counter--;
         if (this.counter <= 0) {
             this.set("message", "Hoorraaay! You unlocked the NativeScript clicker achievement!");
